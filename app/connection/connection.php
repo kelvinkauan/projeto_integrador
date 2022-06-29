953e57f6e42ca915728b210165b7251f8b065375
Kelvin <kelvinkauan9@gmail.com>
2022-06-29 15:49:21 -0300

    app
    implementação do crud aula

---
+++ app/connection/connection.php
@@ -0,0 +1,36 @@
+<?php 
+
+// CRUD MEANS CREATE, READ, UPDATE, DELETE;
+
+class Connection {
+
+
+    // faz conexao com o banco de dados
+
+    private static $host     = "localhost";
+    private static $dbname   = "projeto_integrador_bkm";
+    private static $user     = "root";
+    private static $password = "password#22";
+
+    private static ?PDO $conn = null;
+
+    public static function getConnection(): PDO {
+
+        if(self::$conn == null){
+
+            try {
+                self::$conn = new PDO("mysql:host=".self::$host.";dbname=".self::$dbname, 
+                self::$user, 
+                self::$password);
+
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch(Exception $e) {
+                print("Erro ao conectar com o banco de dados");
+            }
+        }
+
+        return self::$conn;
+    }
+
+}
